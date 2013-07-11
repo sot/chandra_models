7dea6d33a1aeb3b660ec2c741f762d94f41ec4ba
Tom Aldcroft <taldcroft@gmail.com>
2013-07-11 12:41:44 -0400

    Fix project name, url, and descrription in setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 from chandra_models.version import __version__
 
 setup(
-    name='xija_models',
+    name='chandra_models',
     version=__version__,
     author='Matthew Dahmer',
     author_email='matthew.dahmer@gmail.com',
     packages=['chandra_models'],
-    url='http://cxc.cfa.harvard.edu/mta/ASPECT/tool_doc/xija/',
+    url='http://cxc.cfa.harvard.edu/mta/ASPECT/tool_doc/chandra_models/',
     license='BSD',
-    description='Repository for Chandra model parameter files.',
+    description='Repository for Chandra model specification files',
     long_description=open('README').read(),
     package_data={'chandra_models': ['xija/*/*.json']},
 )
