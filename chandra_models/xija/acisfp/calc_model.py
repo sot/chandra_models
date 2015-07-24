12ba90bf97fe4d18c463026de2bdcf3024a2d877
Tom Aldcroft <taldcroft@gmail.com>
2015-07-24 14:06:11 -0400

    Update calc_model to work with the DH heater model

--- chandra_models/xija/acisfp/calc_model.py
+++ chandra_models/xija/acisfp/calc_model.py
@@ -29,6 +29,10 @@ model.comp['sim_px'].set_data(-110.0)
 
 ## INPUT DATA COMPONENTS
 
+# These initializations are used needed for predicting into the future.
+# For analyzing back-orbit data, do not set any of these and let xija
+# grab the right values from telemetry.
+
 # Ephemeris values: position (meters) of Chandra relative to Earth center in
 # ECI coordinate frame.
 model.comp['orbitephem0_x'].set_data(25000e3)  # 25000 km
@@ -50,6 +54,9 @@ model.comp['fep_count'].set_data(6)
 model.comp['vid_board'].set_data(1)
 model.comp['clocking'].set_data(1)
 model.comp['dpa_power'].set_data(0.0)
+
+# Detector housing heater.  Set to True for heater ON, False for heater OFF.
+model.comp['dh_heater'].set_data(True)
 
 model.make()
 model.calc()
